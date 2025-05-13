1d0fdfb6e2151801e033d35dec62a127869d491b
shakhawat.dev <shakhawat.code@gmail.com>
2025-05-13 18:23:43 +0600

    button pointer

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Button({type, text}) {
     return (
         <button
-            className={`py-4 px-10 bg-brand rounded-[10px] w-fit font-montserrat font-bold mt-4 text-[20px] leading-[30px] text-white `}
+            className={`py-4 px-10 bg-brand rounded-[10px] w-fit font-montserrat font-bold mt-4 text-[20px] leading-[30px] text-white cursor-pointer`}
             type={type}>{text}</button>
     );
 }
