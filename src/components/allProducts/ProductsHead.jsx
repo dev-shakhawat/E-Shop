c856a20cb995acb7cbedd5a0e788197ae1385dd6
shakhawat.dev <shakhawat.code@gmail.com>
2025-06-06 17:25:37 +0600

    jani na, hudai

--- src/components/allProducts/ProductsHead.jsx
+++ src/components/allProducts/ProductsHead.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Shortby from '../common/Shortby'
+import ListView from '../../assets/icons/ListView'
+import GridView from '../../assets/icons/GridView'
 
 
 export default function ProductsHead() {
+    const [viewmode , setViewmode] = useState('grid')
   return (
-    <div className='w-full'>
+    <div className='w-full   '>
     <h2 className=" font-poppins font-semibold text-[36px] leading-[46px] text-primary    ">Products</h2>
 
     {/* short by tab */}
-    <div className="flex justify-between">
-        <p className='w-[300px] '>showing 1 - 16 of 160 results</p>
+    <div className="flex justify-between mt-6">
+        <p className='w-[300px] font-montserrat text-base leading-[24px] text-primary '>Showing 1 - 16 of 160 results</p>
 
-        <div className="flex">
+        <div className="flex ">
+
+            {/* short one */}
             <div className="flex gap-4 items-center mr-6 ">
                 <p className="font-montserrat font-normal text-base leading-[24px] text-primary    ">Sort by </p>
                 <Shortby shortsArr={['Popularity', 'Most Recent']} shortWidth={135}/>
             </div>
+
+            {/* short two */}
             <div className="flex gap-4 items-center mx-6">
-                <Shortby shortsArr={['Popularity', 'Most Recent']} shortWidth={200}/>
+                <Shortby shortsArr={['Price Low-to-High', 'Most Recent']} shortWidth={200}/>
             </div>
+
+            {/* short three */}
+            <div className="ml-6 flex items-center gap-4 ">
+                <GridView onClick={() => setViewmode('grid')} className={`w-6 h-6 cursor-pointer `} fill={ viewmode == 'grid' ? `#FF624C` : `#cbcbcb`}/>
+                <ListView onClick={() => setViewmode('list')} className={`w-6 h-6 cursor-pointer `} fill={ viewmode == 'list' ? `#FF624C` : `#cbcbcb`}/>
+            </div>
+
         </div>
     </div>
 </div>
